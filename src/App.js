847a8a1691f45407fd2a9963ffb498c5e7c10d71
isaac.nunes <inunes@iblueconsulting.com.br>
2025-07-18 04:19:52 -0300

    correcao cagda

--- src/App.js
+++ src/App.js
@@ -4,48 +4,40 @@ import './App.css';
 
 // Componentes
 import Navbar from './components/Navbar';
-import {
-  MenuPrincipal,
-  CadastroJovem,
-  ListaJovens,
-  BuscarUsuario,
-  TamanhosCamisa,
-  ParametrosFinanceiros,
-  PrestacaoContas,
-  EditarJovem,
-  Aniversariantes,
-  PrestacaoGeralLista,
-  CaixaGeral
-} from './pages';
+import MenuPrincipal from './pages/MenuPrincipal';
+import CadastroJovem from './pages/CadastroJovem';
+import ListaJovens from './pages/ListaJovens';
+import BuscarUsuario from './pages/BuscarUsuario';
+import TamanhosCamisa from './pages/TamanhosCamisa';
+import ParametrosFinanceiros from './pages/ParametrosFinanceiros';
+import PrestacaoContas from './pages/PrestacaoContas';
+import EditarJovem from './pages/EditarJovem';
+import Aniversariantes from './pages/Aniversariantes'; // ← FALTAVA ESTE IMPORT
+import PrestacaoGeralLista from './pages/PrestacaoGeralLista';
+import CaixaGeral from './pages/CaixaGeral';
 
 function App() {
-  const routes = [
-    { path: "/", element: <MenuPrincipal /> },
-    { path: "/cadastro", element: <CadastroJovem /> },
-    { path: "/lista", element: <ListaJovens /> },
-    { path: "/buscar", element: <BuscarUsuario /> },
-    { path: "/tamanhos-camisa", element: <TamanhosCamisa /> },
-    { path: "/parametros", element: <ParametrosFinanceiros /> },
-    { path: "/prestacao-contas", element: <PrestacaoContas /> },
-    { path: "/editar/:id", element: <EditarJovem /> },
-    { path: "/aniversariantes", element: <Aniversariantes /> },
-    { path: "/prestacao-geral", element: <PrestacaoGeralLista /> },
-    { path: "/caixa-geral", element: <CaixaGeral /> }
-  ];
-
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            {routes.map((route, index) => (
-              <Route key={index} path={route.path} element={route.element} />
-            ))}
-          </Routes>
-        </main>
-      </div>
-    </Router>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<MenuPrincipal />} />
+              <Route path="/cadastro" element={<CadastroJovem />} />
+              <Route path="/lista" element={<ListaJovens />} />
+              <Route path="/buscar" element={<BuscarUsuario />} />
+              <Route path="/tamanhos-camisa" element={<TamanhosCamisa />} />
+              <Route path="/parametros" element={<ParametrosFinanceiros />} />
+              <Route path="/prestacao-contas" element={<PrestacaoContas />} />
+              <Route path="/editar/:id" element={<EditarJovem />} />
+              <Route path="/aniversariantes" element={<Aniversariantes />} /> {/* ← FALTAVA ESTA ROTA */}
+              <Route path="/prestacao-geral" element={<PrestacaoGeralLista />} />
+              <Route path="/caixa-geral" element={<CaixaGeral />} /> {/* Nova rota adicionada */}
+            </Routes>
+          </main>
+        </div>
+      </Router>
   );
 }
 
